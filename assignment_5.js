0130e685497d979b32971c9f7bc051b994a8b2f8
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-04-10 17:54:03 +0200

    Gjorde klart uppgift 5

--- assignment_5.js
+++ assignment_5.js
@@ -1,9 +1,23 @@
 function main() {
     console.log("Uppgift 5.");
 
-    let countries = ["Sweden", "Denmark", "Finland", "Norway"];
     
-    // Er lösning
+
+    let countries = ["Sweden", "Denmark", "Finland", "Norway"];
+
+    let secoundCountry = countries[1];
+    let threeFirstLetters = secoundCountry.substring(0,3)
+    console.log(threeFirstLetters)
+
+    let sumOfAllElements = 0;
+    let meanValue = 0;
+
+    for (let i = 0; i < countries.length; i++){
+        currentCountry = (countries[i]);
+        sumOfAllElements += currentCountry.length
+    }
+    meanValue = sumOfAllElements/countries.length;
+    console.log(meanValue)
 
 }
 

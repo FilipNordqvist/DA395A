e9b9cc3141f75d0cf5b61174c89fa38d165a61d8
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-04-22 08:43:21 +0200

    Klar med uppgift 2

--- assignment-3a/assignment2.js
+++ assignment-3a/assignment2.js
@@ -1,3 +1,15 @@
 //Ta reda på vilken bakgrundsfärg webbplatsen har.
 let  bg = $('body').css('background-color');
-console.log('Karate TANTEN!!')
+console.log('Bakgrundsfärg är: ', bg)
+
+//Ta reda på vilken textstorlek som huvudrubriken har.
+let rubrik= $('h1').css('font-size');
+console.log('Huvudrubriken är storlek: ' , rubrik)
+
+//Ta reda på vilken textstorlek som paragraferna har
+let paragraferna= $('p').css('font-size');
+console.log('Paragraferna har storlek: ' , paragraferna)
+
+//Ta reda på höjden för elementet med
+let heightPrimary= $('#primary').css('height');
+console.log('Höjden för elementet med id="primary" är: ', heightPrimary)

828e469dc129d0f093bee1b7ec30397f835aae9e
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-04-22 20:52:53 +0200

    Klar uppgift 2

--- assignment-3a-start-kit/assignment_2.js
+++ assignment-3a-start-kit/assignment_2.js
@@ -3,6 +3,25 @@ $("#newsletter-form").on("submit", function(e) {
     // Hindra formuläret från att skicka iväg användaren.
     e.preventDefault();
 
+    if ($("#name-field").val().length <= 0){
+        $("#name-field").css("background-color", "red");
+    }else{
+        var name = $("#name-field").val();
+        $("#name-field").css("background-color", "white");
+    }
+    if ($("#age-field").val().length <= 0){
+        $("#age-field").css("background-color", "red");
+    }else{
+        var age = $("#age-field").val();
+        $("#age-field").css("background-color", "white");
+    }
+    if ($("#email-field").val().length <= 0){
+        $("#email-field").css("background-color", "red");
+    }else{
+        var epost = $("#email-field").val();
+        $("#email-field").css("background-color", "white");
+    }
+  
     // Här placerar ni er validerings kod.
 
     // Om allting var korrekt - skicka vidare användaren.

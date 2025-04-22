59c02eb1429ea6886573f8e9419ebffeb7332abe
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-04-22 20:25:23 +0200

    KLar uppgift 1

--- assignment-3a-start-kit/assignment_1.js
+++ assignment-3a-start-kit/assignment_1.js
@@ -1,11 +1,7 @@
-$("#number-1, #number-2").keyup(function(){  
-    var number1 = $("#number-1").val();
-    var number2  = $("#number-2").val();
-    
-    
-    $("#result-box").val(number1 * number2);
-    
+$("#number-1, #number-2").keyup(function () {
+  if ($("#number-1").val().length > 0 && $("#number-2").val().length > 0) {
+    $("#result-box").val($("#number-1").val() * $("#number-2").val());
+  } else {
+    $("#result-box").val(" ");
+  }
 });
-
-
-

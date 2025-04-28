9c92946be25802a68aa7de6dd26e30dab3f07d08
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-04-28 19:06:58 +0200

    Kom på att jag kunde gå till förälder!!

--- assignment-4a/script.js
+++ assignment-4a/script.js
@@ -1,10 +1,6 @@
 function printMovies(movies) {
-    
-    //movies.forEach((element) => console.log(element));
-
-
-
-    /*
+  movies.forEach((element) => console.log(element));
+  /*
         Todo: Skriv ut alla filmer i arrayen "movies". Varje film ska ha följande format:
 
         <li data-grade="5" data-title="Star Wars">
@@ -17,68 +13,114 @@ function printMovies(movies) {
             <img src="delete.png" alt="Delete movie" class="delete-movie">
         </li>
     */
-
-
 }
 
-function loadMovies() {
-    /*
+renderMovies();
+
+function getMovies() {
+  /*
         Todo: Filmerna ska snart laddas in från localStorage, men till
         en början nöjer vi oss med en vanlig array med filmer
     */
-        let jsonMovies = localStorage.getItem("movies");
-        let movies = JSON.parse(jsonMovies);
-        console.log("Loaded movies: ", movies);
-    
-    
-    /*    const movies = [
+  var movies = localStorage.getItem("movies");
+  // Kontrollera om det finns några filmer i localStorage
+  if (movies === null) {
+    console.log("movies är tom!")
+    // Det finns inget i localStorage, så vi skapar en tom lista där
+    localStorage.setItem("movies", JSON.stringify([]));
+    // Returnerar en tom lista (= inga todos)
+    return [];
+  } else {
+    // Returnerar alla todos i en lista (från JSON => lista med objekt)
+
+    return JSON.parse(movies);
+  }
+
+  /*    const movies = [
         { title: "Star Wars", grade: 5 },
         { title: "Titanic", grade: 4 },
         { title: "Drive", grade: 1 }
     ];*/
-
-    return movies;
 }
 
 function getStars(rating) {
-    /*
+  /*
         Todo: Baserat på "rating" (en siffra) så returnerar denna
         funktion HTML-kod för antalet stjärnor (betyg) för en film.
     */
 }
 
-function saveMovies(movies) {
-    //Börja här-->
+function renderMovies() {
 
-    /*
+  var movies = getMovies();
+  const moviesList = $("#movie-list");
+
+  $("#movie-list ul").html("");
+
+  for (var i = 0; i < movies.length; i++) {
+    const list = $("<li></li>")
+      .attr("data-grade", movies[i].grade)
+      .attr("data-title", movies[i].title)
+      .text(movies[i].title);
+
+    const stars = Array.from({ length: parseInt(movies[i].grade) }, () =>
+      $("<img>").attr("src", "./images/star.png").attr("alt", "Star")
+    );
+
+    const deleteMovie = $("<img>")
+      .attr("src", "./images/delete.png")
+      .attr("alt", "Delete movie")
+      .attr("class", "delete-movie")
+
+
+      list.append(deleteMovie);
+      list.append(stars);
+      moviesList.append(list)
+  }
+
+
+  //Börja här-->
+
+  /*
         Todo: Sparar filmerna till localStorage (JSON-format)
-    */}
+    */
+}
 
 $("#new-movie-form").on("submit", function (e) {
-    e.preventDefault();
-
-    let title = "";
-    let grade = "";
   
-    if ($("#title").val().length > 0) {
-      title = $("#title").val();
-    } else {
-      alert("Ingen titel angiven");
-    }
-    if ($("#grade").val() > 0 && $("#grade").val() <= 5) {
-      grade = $("#grade").val();
-    } else {
-      alert("Ingen rating angiven");
-    }
+e.preventDefault();
 
-    const movie= {title:title, grade: grade}
-    localStorage.setItem("movies",JSON.stringify(movie));
+  var title = "";
+  var grade = "";
 
-    saveMovies(movie);
+  
+  
+  if ($("#title").val().length > 0) {
+    title = $("#title").val();
+  } else {
+    alert("Ingen titel angiven");
+    return;
+  }
+  if ($("#grade").val() > 0 && $("#grade").val() <= 5) {
+    grade = $("#grade").val();
+  } else {
+    alert("Ingen rating angiven");
+    return;
+  }
 
-    $("#new-movie-form").trigger("reset");
-    
-    /*
+  var moviesStorage = getMovies();
+  moviesStorage.push({
+    grade:grade,
+    title:title
+  });
+
+  localStorage.setItem("movies", JSON.stringify(moviesStorage));
+
+  renderMovies();
+
+  $("#new-movie-form").trigger("reset");
+
+  /*
         Todo: När man klickar på knappen "Spara film" så ska funktionen:
         1. Validera att man skrivit in en titel & valt ett betyg. Om inte
         så ska vi visa upp (i en popup-ruta) ett meddelande om att både
@@ -87,27 +129,28 @@ $("#new-movie-form").on("submit", function (e) {
         3. Visa den nya filmen i vår lista av filmer
         4. Återställa formuläret (ingen titel, eller betyg valt)
     */
-        
 });
 
-
 $("#order-alphabetic").on("click", function () {
-    /*
+  /*
         Todo: Sortera filmlistan alfabetiskt stigande
     */
 });
 
 $("#order-grade").on("click", function () {
-    /*
+  /*
         Todo: Sortera filmlistan fallande efter betyg
     */
 });
 
-$("#movie-list").on("click", ".delete-movie", function () {
+$("#movie-list").on("click", ".delete-movie", function () {   
+    const value = $(this).parent().data('title');
     
-    localStorage.removeItem("movies");
-    //loadMovies();
-    /*
+    
+
+   
+  
+  /*
         Todo: Ska ta bort en film från:
         1. Filmlistan i localStorage
         2. Från filmlistan som visas på webbsidan
@@ -116,6 +159,6 @@ $("#movie-list").on("click", ".delete-movie", function () {
 
 // Skriver ut filmerna i vår lista när sidan laddats klart
 $(document).ready(function () {
-    const movies = loadMovies();
-    printMovies(movies);
+  const movies = getMovies();
+  printMovies(movies);
 });

173b8f310d9ba97fb99322750cd3255228131273
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-05-07 13:40:31 +0200

    Lade upp min gamla lösnming av uppgift 3 del 3

---
+++ assigment 6/assignment_6.js
@@ -0,0 +1,43 @@
+$("#add-movie-form").on("submit", function (e) {
+  // Hindra formuläret från att skicka iväg användaren.
+  e.preventDefault();
+
+  let titel = "";
+  let rating = "";
+
+  if ($("#title-field").val().length > 0) {
+    titel = $("#title-field").val();
+  } else {
+    alert("Ingen titel angiven");
+  }
+  if ($("#rating-field").val() > 0 && $("#rating-field").val() <= 5) {
+    rating = $("#rating-field").val();
+  } else {
+    alert("Ingen rating angiven");
+  }
+  addToMovieList(titel, rating);
+
+  $("#add-movie-form").trigger("reset");
+});
+
+function addToMovieList(titel, rating) {
+  const movies = $("#movies");
+  const list = $("<li></li>")
+    .attr("data-title", titel)
+    .attr("data-grade", rating)
+    .text(titel);
+  const stars = Array.from({ length: parseInt(rating) }, () =>
+    $("<img>").attr("src", "./images/star.png").attr("alt", "Star")
+  );
+  const deleteMovie = $("<img>")
+    .attr("src", "./images/delete.png")
+    .attr("alt", "Delete movie")
+    .attr("class", "delete-movie-icon")
+    .on("click", function () {
+      list.remove();
+    });
+
+  movies.append(list);
+  list.append(deleteMovie);
+  list.append(stars);
+}
